8c00a9165cac2d2b7dcea798a8b1cae32050340e
oliver <loliverhennigh101@gmail.com>
2016-07-25 19:02:01 -0400

    tfrecord generation done

--- utils/createTFRecords.py
+++ utils/createTFRecords.py
@@ -3,13 +3,15 @@
 import numpy as np 
 import tensorflow as tf 
 import cv2 
+import sys
 from glob import glob as glb
+sys.path.append("game/")
 from Atari import Atari
 import random
 
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_string('atari_game', 'space_invaders',
+tf.app.flags.DEFINE_string('atari_game', 'space_invaders.bin',
                             """name of atari game to run""")
 tf.app.flags.DEFINE_integer('num_training_frames', 10000,
                             """name of atari game to run""")
@@ -20,30 +22,32 @@ def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 def get_converted_frame(atari, shape, color):
-  for i in xrange(FLAGS.video_frames_per_train_frame):
-    ret, frame = cap.read()
-  frame = cv2.resize(frame, shape, interpolation = cv2.INTER_CUBIC)
+  action = random_action(len(atari.legal_actions))
+  atari.next(action)
+  observation, reward, terminal = atari.next(action)
+  observation = cv2.resize(observation, shape, interpolation = cv2.INTER_CUBIC)
   if color:
-    return frame
+    return observation, reward, action 
   else:
     return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-def random_action(num_actions)
+def random_action(num_actions):
   random_action = np.zeros(num_actions)
-  action_ind = random.randint(0, num_actions)
+  action_ind = random.randint(0, num_actions-1)
   random_action[action_ind] = 1
   return random_action
 
 def generate_tfrecords(seq_length, shape, frame_num, color):
   # make atari game
+  print(FLAGS.atari_game)
   atari = Atari(FLAGS.atari_game) 
   num_actions = len(atari.legal_actions)
   
   # create tf writer
-  record_filename = '../data/tfrecords/' + FLAGS.atari_game + '/' + FLAGS.atari_game + '_seq_' + str(seq_length) + '_size_' + str(shape[0]) + 'x' + str(shape[1]) + 'x' + str(frame_num) + '_color_' + str(color) + '.tfrecords'
+  record_filename = '../data/tfrecords/' + FLAGS.atari_game[:-4] + '/' + FLAGS.atari_game.replace('.', '_') + '_seq_' + str(seq_length) + '_size_' + str(shape[0]) + 'x' + str(shape[1]) + 'x' + str(frame_num) + '_color_' + str(color) + '.tfrecords'
  
   # check to see if file alreay exists 
-  tfrecord_filename = glb('../data/tfrecords/'+FLAGS.video_dir+'/*')
+  tfrecord_filename = glb('../data/tfrecords/'+FLAGS.atari_game[:-4]+'/*')
   if record_filename in tfrecord_filename:
     print('already a tfrecord there! I will skip this one')
     return 
@@ -73,7 +77,7 @@ def generate_tfrecords(seq_length, shape, frame_num, color):
   
   print('now generating tfrecords for ' + FLAGS.atari_game + ' and saving to ' + record_filename)
 
-  for _ in len(num_training_frames):
+  for _ in xrange(FLAGS.num_training_frames):
     # create frames
     if ind == 0:
       for s in xrange(seq_length):
@@ -82,32 +86,31 @@ def generate_tfrecords(seq_length, shape, frame_num, color):
             if color:
               frames[:,:,i*3:(i+1)*3], reward, action = get_converted_frame(atari, shape, color)
             else:
-              frames[:,:,i] = get_converted_frame(cap, shape, color)
+              frames[:,:,i], reward, action = get_converted_frame(cap, shape, color)
 
           ind = ind + 1
         else:
           if color:
             frames[:,:,0:frame_num*3-3] = frames[:,:,3:frame_num*3]
-            frames[:,:,(frame_num-1)*3:frame_num*3] = get_converted_frame(cap, shape, color)
+            frames[:,:,(frame_num-1)*3:frame_num*3], reward, action = get_converted_frame(atari, shape, color)
 
           else:
             frames[:,:,0:frame_num-1] = frames[:,:,1:frame_num]
-            frames[:,:,frame_num-1] = get_converted_frame(cap, shape, color)
+            frames[:,:,frame_num-1], reward, action = get_converted_frame(atari, shape, color)
 
         seq_frames[s, :, :, :] = frames[:,:,:]
     else:
       if color:
         frames[:,:,0:frame_num*3-3] = frames[:,:,3:frame_num*3]
-        frames[:,:,(frame_num-1)*3:frame_num*3] = get_converted_frame(cap, shape, color)
+        frames[:,:,(frame_num-1)*3:frame_num*3], reward, action = get_converted_frame(atari, shape, color)
 
       else:
         frames[:,:,0:frame_num-1] = frames[:,:,1:frame_num]
-        frames[:,:,frame_num-1] = get_converted_frame(cap, shape, color)
+        frames[:,:,frame_num-1], reward, action = get_converted_frame(atari, shape, color)
 
       seq_frames[0:seq_length-1,:,:,:] = seq_frames[1:seq_length,:,:,:]
       seq_frames[seq_length-1, :, :, :] = frames[:,:,:]
 
-    #print(seq_frames.shape)
 
     # process frame for saving
     seq_frames = np.uint8(seq_frames)
@@ -123,14 +126,13 @@ def generate_tfrecords(seq_length, shape, frame_num, color):
     writer.write(example.SerializeToString()) 
 
     # Display the resulting frame
-    cv2.imshow('frame',seq_frames[0,:,:,0:3])
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    #cv2.imshow('frame',seq_frames[0,:,:,0:3])
+    #if cv2.waitKey(1) & 0xFF == ord('q'):
+    #    break
  
     # print status
-    ind = ind + 1
-    if ind%1000 == 0:
-      print('percent converted = ' + str(100.0 * float(converted_frames) / float(total_num_frames)))
+    if _%100 == 0:
+      print('percent converted = ' + str(100.0 * float(_) / float(FLAGS.num_training_frames)))
 
   # When everything done, release the capture
   cap.release()
---
+++ utils/test.py
@@ -0,0 +1,7 @@
+
+
+import createTFRecords
+
+
+createTFRecords.generate_tfrecords(1, (84,84), 3, True)
+
